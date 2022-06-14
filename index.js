bbd4069e68c8b15c1ad568c7731db4751068e1d2
Coni2206 <nicolas.2206@hotmail.com>
2022-06-14 16:47:53 -0300

    funcionalidad del boton remove

--- index.js
+++ index.js
@@ -4,15 +4,48 @@ document.addEventListener("DOMContentLoaded", () => {
     const table= document.getElementById("table");
     const alert= document.getElementById("alert");
     const btn = document.getElementById("add");
+    let id = 1;
 
-    
+    function removeTodo(id) {
+        document.getElementById(id).remove();
+    };
+
     function addTodo() {
         if (description.value === "" || title.value === "") {
             alert.classList.remove("d-none");
-            alert.innerText = "titulo y descripcion son requeridos"
-        } else {
-            console.log("ok")
+            alert.innerText = "titulo y descripcion son requeridos";
+            return;
+        } 
+        alert.classList.add("d-none");
+        const row = table.insertRow();
+        row.setAttribute("id", id++);
+        row.innerHTML = `
+        <td>
+            ${title.value}
+        </td>
+        <td>
+            ${description.value}
+        </td>
+        <td class="text-center">
+            <input type="checkbox">
+        </td>
+        <td class="text-right">
+            <button class="btn btn-primary mb-1">
+            <i class="fa fa-pencil"></i>
+            </button> 
+        </td>
+        `;
+
+        const removeBtn = document.createElement("button");
+        removeBtn.classList.add("btn", "btn-danger", "mb-1", "ml-1");
+        removeBtn.innerHTML = "<i class='fa fa-trash'></i>"
+        removeBtn.onclick = function(e){
+            removeTodo(row.getAttribute("id"));
         }
+        row.children[3].appendChild(removeBtn);
+
+         
+
     };
     
     
